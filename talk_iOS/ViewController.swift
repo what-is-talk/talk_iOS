0a444a2ec13dc49dfcaa26a76c31721c93ea6d7c
박지수 <jisupark2000@gmail.com>
2023-01-03 22:02:35 +0900

    프로젝트 생성

---
+++ talk_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  talk_iOS
+//
+//  Created by User on 2023/01/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
