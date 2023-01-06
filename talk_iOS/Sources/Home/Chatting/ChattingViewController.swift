e3be7d58d0cc268f66f827403b570f81269f0194
박지수 <jisupark2000@gmail.com>
2023-01-06 20:56:34 +0900

    프로젝트 폴더 구조 정리

---
+++ talk_iOS/Sources/Home/Chatting/ChattingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ChattingViewController.swift
+//  talk_iOS
+//
+//  Created by User on 2023/01/06.
+//
+
+import UIKit
+
+class ChattingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
