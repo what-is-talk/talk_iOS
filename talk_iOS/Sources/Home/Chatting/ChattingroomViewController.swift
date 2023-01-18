bb3bbdb75afd6547331be9eb8f29c40708e2bce6
yoogangee <cathyou1227@gmail.com>
2023-01-18 16:43:37 +0900

    [Feat] 채팅방 UI구현

--- talk_iOS/Sources/Home/Chatting/ChattingroomViewController.swift
+++ talk_iOS/Sources/Home/Chatting/ChattingroomViewController.swift
@@ -4,32 +4,29 @@
 //
 //  Created by 경유진 on 2023/01/09.
 //
+// 텍스트필드 - msgTextField , 전송버튼 - sendButton
 
 import UIKit
 import SnapKit
-import Alamofire
-// 안녕,,,
+
 class ChattingroomViewController: UIViewController, UITextFieldDelegate {
-    
-    @IBOutlet weak var msgTextField: UITextField!
-    @IBOutlet weak var sendButton: UIButton!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.initTitle()
         self.leftButton()
         self.rightButton()
-        //self.msgfield()
-        
-        self.msgTextField.delegate = self
+        setUpView()
+        setConstraints()
         
         view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap(sender:))))
-
     }
     
+    // 네비게이션
     // 네비게이션 타이틀
     func initTitle() {
+        view.backgroundColor = .white
+
         let containerView = UIView(frame: CGRect(x: 0, y: 0, width: 200, height: 36))
         
         let topTitle = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 18))
@@ -51,29 +48,133 @@ class ChattingroomViewController: UIViewController, UITextFieldDelegate {
         
         self.navigationItem.titleView = containerView
     }
-    
+    // 뒤로가기 버튼
     func leftButton() {
         let backButton =  UIBarButtonItem(image: UIImage(named: "Vector.png"), style: .done, target: self, action: nil)
         backButton.tintColor = .black
         navigationItem.leftBarButtonItem = backButton
     }
-    
+    // 검색 버튼
     func rightButton() {
         let searchButton =  UIBarButtonItem(image: UIImage(named: "Vector-2.png"), style: .done, target: self, action: nil)
         searchButton.tintColor = .black
         navigationItem.rightBarButtonItem = searchButton
     }
     
-    // ? 뭔가 이상함
-    /*
-    func msgfield() {
-        msgTextField.layer.backgroundColor = UIColor(red: 0.988, green: 0.988, blue: 0.988, alpha: 1).cgColor
-        msgTextField.layer.cornerRadius = 30
-        msgTextField.layer.borderWidth = 1
-        msgTextField.layer.borderColor = UIColor(red: 0.094, green: 0.078, blue: 0.255, alpha: 0.2).cgColor
-    }
-    */
     
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        return tableView
+    }()
+    
+    private let user: UILabel = {
+        let user = UILabel()
+        user.text = "사용자 A"
+        user.textColor = UIColor(red: 0.271, green: 0.553, blue: 0.784, alpha: 1)
+        user.font = UIFont(name: "SFPro-Semibold", size: 14)
+        return user
+    }()
+    
+    private let message: UITextView = {
+        let message =  UITextView()
+        message.textColor = .black
+        message.font = UIFont(name: "SFPro-Regular", size: 16)
+        return message
+    }()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    lazy var plusButton: UIButton = {
+        let plusButton = UIButton()
+        let plusimage = UIImage(named: "Vector-3.png")
+        plusButton.setBackgroundImage(plusimage, for: .normal)
+        //plusButton.addTarget(self, action: Selector(("openview")), for: .touchUpInside)
+        return plusButton
+    }()
+    /*
+    func openview() {
+        self.plusButton.transform = CGAffineTransform(rotationAngle: .pi*0.25)
+    }*/
+    
+    // ? 뭔가 이상함
+    lazy var textField: UITextField = {
+        //textField.frame = CGRect(x: 0, y: 0, width: 80, height: 30)
+        let textField = UITextField()
+        // Substitute the characters to be displayed.
+        textField.placeholder = "메시지를 입력하세요."
+        
+        // Set Delegate to itself
+        textField.delegate = self
+        
+        // Display frame.
+        textField.layer.cornerRadius = 17.5
+        textField.layer.borderWidth = 1
+        textField.layer.borderColor = UIColor(red: 0.094, green: 0.078, blue: 0.255, alpha: 0.2).cgColor
+            
+        // 왼쪽 여백
+        textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: 0))
+        textField.leftViewMode = .always
+        
+        // 오른쪽 버튼
+        let sendButton = UIButton()
+        let sendimage = UIImage(named: "send.png")
+        sendButton.setBackgroundImage(sendimage, for: .normal)
+        sendButton.frame = CGRectMake(0,0, 20, 20)
+        //sendButton.addTarget(self, action: "sendMessage", for: .touchUpInside)
+        textField.rightView = sendButton
+        textField.rightViewMode = .whileEditing
+        
+        return textField
+    }()
+    
+    lazy var bottomView: UIView = {
+        let bottomView = UIView()
+        bottomView.backgroundColor = .clear
+        bottomView.addSubview(plusButton)
+        bottomView.addSubview(textField)
+        return bottomView
+    }()
+    
+    func setUpView() {
+        self.view.addSubview(tableView)
+        self.view.addSubview(bottomView)
+    }
+    
+    func setConstraints() {
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(self.view.safeAreaLayoutGuide)
+            make.right.left.equalTo(self.view.safeAreaLayoutGuide)
+            make.bottom.equalTo(self.view.safeAreaLayoutGuide).inset(44)
+        }
+        bottomView.snp.makeConstraints { make in
+            make.top.equalTo(tableView.snp.bottom)
+            make.right.left.equalTo(self.view.safeAreaLayoutGuide)
+            make.bottom.equalTo(self.view.safeAreaLayoutGuide)
+        }
+        textField.snp.makeConstraints { make in
+            make.width.equalTo(333)
+            make.height.equalTo(35)
+            make.top.equalToSuperview().offset(4)
+            make.left.equalToSuperview().offset(46)
+            make.right.equalToSuperview().offset(-11)
+            make.bottom.equalToSuperview().offset(-5)
+        }
+        plusButton.snp.makeConstraints { make in
+            //make.width.height.equalTo(18)
+            make.top.equalToSuperview().offset(13.19)
+            make.right.equalTo(self.textField.snp.left).offset(-13.58)
+            make.left.equalToSuperview().offset(15.58)
+            make.bottom.equalToSuperview().offset(-13.98)
+        }
+    }
+    
+
     // 키보드 변경 확인하는 옵져버를 등록
     override func viewWillAppear(_ animated: Bool) {
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardUp), name: UIResponder.keyboardWillShowNotification, object: nil)
@@ -110,6 +211,32 @@ class ChattingroomViewController: UIViewController, UITextFieldDelegate {
         sender.cancelsTouchesInView = false
     }
     
-    // 전송 버튼 숨기기&나타내기
+    // Return키 눌렀을 때 키보드 내려가게
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+            self.view.endEditing(true)
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+            textField.resignFirstResponder() // TextField 비활성화
+            return true
+    }
+}
+
+import SwiftUI
+
+struct ViewControllerRepresentable: UIViewControllerRepresentable {
+    typealias UIViewControllerType = ChattingroomViewController
     
+    func makeUIViewController(context: Context) -> ChattingroomViewController {
+            return ChattingroomViewController()
+        }
+
+        func updateUIViewController(_ uiViewController: ChattingroomViewController, context: Context) {
+        }
+}
+
+@available(iOS 13.0.0, *)
+struct ViewPreview: PreviewProvider {
+    static var previews: some View {
+            ViewControllerRepresentable()
+    }
 }
