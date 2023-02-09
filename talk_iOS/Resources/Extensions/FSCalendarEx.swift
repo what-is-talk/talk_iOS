5134e2c8621547295ce6a57d0e3cc3d4825e437e
yoogangee <cathyou1227@gmail.com>
2023-02-09 21:29:19 +0900

    Calendar Main, Add 페이지 UI 완성

--- talk_iOS/Resources/Extensions/FSCalendarEx.swift
+++ talk_iOS/Resources/Extensions/FSCalendarEx.swift
@@ -7,15 +7,22 @@
 
 import Foundation
 import FSCalendar
-/*
+
 extension CalendarMainViewController {
     // 날짜 선택 시 콜백 메소드
-        func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
-            print(dateFormatter.string(from: date) + " 선택됨")
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        print(dateFormatter.string(from: date) + " 선택됨")
+        selectedDate = dateFormatter.string(from: date)
+    }
+    // 날짜 선택 해제 시 콜백 메소드
+    public func calendar(_ calendar: FSCalendar, didDeselect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        print(dateFormatter.string(from: date) + " 해제됨")
+        selectedDate = ""
+    }
+    func calendar(_ calendar: FSCalendar, numberOfEventsFor date: Date) -> Int {
+        if self.eventsArray.contains(date) {
+            return 1
         }
-        
-        // 날짜 선택 해제 시 콜백 메소드
-        public func calendar(_ calendar: FSCalendar, didDeselect date: Date, at monthPosition: FSCalendarMonthPosition) {
-            print(dateFormatter.string(from: date) + " 해제됨")
-        }
-}*/
+        return 0
+    }
+}
