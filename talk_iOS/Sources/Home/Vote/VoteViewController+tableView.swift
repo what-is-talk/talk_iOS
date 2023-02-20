516d0a75895bb76434fdaa534096a33712e98006
_yoonyoon_1 <heeyoon1302@naver.com>
2023-02-20 12:04:50 +0900

    [FEAT] 투표 기능 초기 UI 구현

---
+++ talk_iOS/Sources/Home/Vote/VoteViewController+tableView.swift
@@ -0,0 +1,19 @@
+//
+//  VoteViewController+tableView.swift
+//  talk_iOS
+//
+//  Created by 김희윤 on 2023/02/20.
+//
+
+import UIKit
+
+class VoteViewController_tableView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
